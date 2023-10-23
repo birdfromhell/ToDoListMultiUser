f5220e2c8829bc1550015076b7fa79e93e810f1b
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-23 21:44:13 +0700

    Add Todo feature for user in FastAPI
    This commit introduces a Todo feature for each user in our FastAPI application. In the changes, 'typing' module was imported and added 'Todo' class making it possible to create and read todos for each user. This change enhances user management and task tracking in the application.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -6,13 +8,16 @@ app = FastAPI()
 users = {}
 todos = {}
 
+
 class User(BaseModel):
     name: str
     password: str
 
+
 class Todo(BaseModel):
     title: str
     description: str
+
 
 @app.post("/users/", response_model=User)
 def create_user(user: User):
@@ -20,6 +25,14 @@ def create_user(user: User):
     return user
 
 
+@app.post("/users/{username}/todos/", response_model=Todo)
+def create_todo_for_user(username: str, todo: Todo):
+    if username not in todos:
+        todos[username] = []
+    todos[username].append(todo)
+    return todo
 
 
-
+@app.get("/users/{username}/todos/", response_model=List[Todo])
+def read_todos_for_user(username: str):
+    return todos.get(username, [])
