7e76c8eae9097e8a962d60cb59dcac9d941d2695
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-23 22:19:52 +0700

    Add exception handling to CRUD operations
    This commit introduces try-except blocks within four CRUD operation functions: get_user_by_name, create_user, create_todo, and get_todos_by_user. These changes are necessary to handle any unexpected errors without crashing the application and provide helpful error messages to the user. Specifically, an IntegrityError is now caught and managed in create_user and create_todo functions to handle scenarios where duplicate entries are attempted. Password hashing has also been implemented during user creation.

--- crud.py
+++ crud.py
@@ -1,27 +1,51 @@
 from sqlalchemy.orm import Session
 from models import UserInDB, TodoInDB
 from schemas import UserCreate, TodoCreate
-
+from sqlalchemy.exc import IntegrityError
 
 def get_user_by_name(db: Session, name: str):
-    return db.query(UserInDB).filter(UserInDB.name == name).first()
+    try:
+        query = db.query(UserInDB).filter(UserInDB.name == name)
+        result = query.first()
+        if result is None:
+            return "User not found."
+        return result
+    except Exception as e:
+        return str(e)
 
 
 def create_user(db: Session, user: UserCreate):
-    db_user = UserInDB(name=user.name, password=user.password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    hashed_password = hash_password(user.password)  # Implement this function
+    db_user = UserInDB(name=user.name, password=hashed_password)
+    try:
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except IntegrityError:
+        db.rollback()
+        return "User already exists."
+    except Exception as e:
+        return str(e)
 
 
 def create_todo(db: Session, todo: TodoCreate, user_name: str):
     db_todo = TodoInDB(**todo.dict(), owner=user_name)
-    db.add(db_todo)
-    db.commit()
-    db.refresh(db_todo)
-    return db_todo
+    try:
+        db.add(db_todo)
+        db.commit()
+        db.refresh(db_todo)
+        return db_todo
+    except IntegrityError:
+        db.rollback()
+        return "Todo already exists."
+    except Exception as e:
+        return str(e)
 
 
 def get_todos_by_user(db: Session, username: str):
-    return db.query(TodoInDB).filter(TodoInDB.owner == username).all()
+    try:
+        query = db.query(TodoInDB).filter(TodoInDB.owner == username).all()
+        return query
+    except Exception as e:
+        return str(e)
